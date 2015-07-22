721015d5a7ea9745094f06dfcea3625c20555992
Louis Taylor <louis@kragniz.eu>
2015-07-22 02:09:48 +0000

    Check number is the field that is different

--- inidiff/tests/test_diff.py
+++ inidiff/tests/test_diff.py
@@ -19,3 +19,8 @@ class TestDiff(unittest.TestCase):
 
     def test_some_differences(self):
         self.assertTrue(len(inidiff.diff(INI_1, INI_2)) > 0)
+
+    def test_number_is_different(self):
+        diffs = inidiff.diff(INI_1, INI_2)
+        first, second = diffs[0]
+        self.assertEqual('number', first[1])

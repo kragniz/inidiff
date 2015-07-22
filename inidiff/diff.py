caf7e6c0fb33622bc9e0697c967589df65f33764
Louis Taylor <louis@kragniz.eu>
2015-07-22 01:02:40 +0000

    Enable flake8

--- inidiff/diff.py
+++ inidiff/diff.py
@@ -1,2 +1,3 @@
 def diff(first, second):
+    """Diff two ini files."""
     return []
--- inidiff/tests/test_diff.py
+++ inidiff/tests/test_diff.py
@@ -6,6 +6,9 @@ INI_1 = '''[test]
 number=10
 '''
 
+
 class TestDiff(unittest.TestCase):
+    """Test diffs diff things."""
+
     def test_no_differences(self):
         self.assertEqual([], inidiff.diff(INI_1, INI_1))

63b6d20882076325cb792da5b9714cc8c20adb42
Louis Taylor <louis@kragniz.eu>
2015-07-22 02:37:59 +0000

    Add PermissiveConfigParser

--- inidiff/diff.py
+++ inidiff/diff.py
@@ -2,9 +2,20 @@ from six.moves import cStringIO
 import six.moves.configparser as configparser
 
 
+class PermissiveConfigParser(configparser.SafeConfigParser):
+    """A SafeConfigParser that returns empty instead of erroring."""
+
+    def options(self, section):
+        if self.has_section(section):
+            # old style python classes are dumb
+            return configparser.SafeConfigParser.options(self, section)
+        else:
+            return []
+
+
 def conf_from_str(conf_str):
     """Return a ConfigParser object from a string."""
-    parser = configparser.SafeConfigParser()
+    parser = PermissiveConfigParser()
     ini_str = cStringIO(conf_str)
     parser.readfp(ini_str)
 

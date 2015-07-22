4822118cb0bb11fe142fb2661b71d3828982209b
Louis Taylor <louis@kragniz.eu>
2015-07-22 02:01:43 +0000

    Compare options

--- inidiff/diff.py
+++ inidiff/diff.py
@@ -1,3 +1,31 @@
+from six.moves import cStringIO
+import six.moves.configparser as configparser
+
+
+def conf_from_str(conf_str):
+    """Return a ConfigParser object from a string."""
+    parser = configparser.SafeConfigParser()
+    ini_str = cStringIO(conf_str)
+    parser.readfp(ini_str)
+
+    return parser
+
+
 def diff(first, second):
     """Diff two ini files."""
-    return []
+    conf_first = conf_from_str(first)
+    conf_second = conf_from_str(second)
+
+    diffs = []
+
+    sections = set(conf_first.sections() + conf_second.sections())
+    for section in sections:
+        options = set(conf_first.options(section) +
+                      conf_second.options(section))
+        for option in options:
+            f = conf_first.get(section, option)
+            s = conf_second.get(section, option)
+            if f != s:
+                diffs.append(((section, option, f), (section, option, s)))
+
+    return diffs
--- inidiff/tests/test_diff.py
+++ inidiff/tests/test_diff.py
@@ -6,9 +6,16 @@ INI_1 = '''[test]
 number=10
 '''
 
+INI_2 = '''[test]
+number=20
+'''
+
 
 class TestDiff(unittest.TestCase):
     """Test diffs diff things."""
 
     def test_no_differences(self):
         self.assertEqual([], inidiff.diff(INI_1, INI_1))
+
+    def test_some_differences(self):
+        self.assertTrue(len(inidiff.diff(INI_1, INI_2)) > 0)
